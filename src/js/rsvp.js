6647f36a646185de87d01390663dcb624b0eb4f8
bgibz <brendanjamesgibson@gmail.com>
2020-05-06 14:13:13 -0700

    update find invitation form

--- src/js/rsvp.js
+++ src/js/rsvp.js
@@ -7,16 +7,17 @@ function getRSVP() {
         guestName = guestName.trim();
         guestName = guestName.replace("+", " ");
         console.log("Guest: " + guestName);
+        //TODO: Display loading gif??
         $.ajax({
             url: "https://h4467dph4g.execute-api.us-west-2.amazonaws.com/develop/RSVP?guest_name="+guestName,
             context: this,
             cache: false,
             method: "GET",
             dataType: "json",
-            headers: {"Accept": "application/json; odata=verbose",
-                    },
+            headers: {"Accept": "application/json; odata=verbose"},
             success: function(data) {
-                console.log("Data: " + data)
+                $('#nameCheck').hide();
+                displayRSVP(data.result);
             },
             error: function(data) {
                 console.log("Error: " + JSON.stringify(data))
@@ -24,6 +25,30 @@ function getRSVP() {
         $("#submitError").append("<span class='text-danger'>Something went wrong. Please try again later.</span>")
             }
         })
+    }
+}
+
+function displayRSVP(data) {
+    var i;
+    for (i = 0; i < data.length; i++) {
+        if (data[i].Response == 'Pending') {
+            $("#formTitle").empty();
+            $("#formTitle").text("Please RSVP By August 7th");
+            let card = $("<div class='form-group rsvp'></div>");
+            let id = $("<input class='_id' type='hidden' value='" + data[i]._oid + "'>");
+            let name = $("<h4>" + data[i].Guest + "</h4>");
+            let radioDiv = $("<div class='form-check form-check-inline'></div>");
+            let radioDivTwo = $("<div class='form-check form-check-inline'></div>");
+            let respY = $("<input type='radio' name='rsvp' id ='rsvpYes" + i + "'" + "value='Yes'>");
+            let labelY = $("<label class='form-check-label' for='rsvpYes" + i + "'" + ">Accepts With Pleasure</label>");
+            let respN = $("<input type='radio' name='rsvp' id ='rsvpNo" + i + "'" + "value='No'>");
+            let labelN = $("<label class='form-check-label' for='rsvpNo" + i + "'" + ">Regretfully Declines</label>");
+            radioDiv.append(respY, labelY);
+            radioDivTwo.append(respN, labelN);
+            card.append(id, name, radioDiv, radioDivTwo);
+            $("#rsvpForm").append(card);
+            $("#responseDiv").show();
+        }
     }
 }
 
