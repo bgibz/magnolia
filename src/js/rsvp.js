4ff1b93962ef29563adc80ea1ed2bfc41c3c893f
bgibz <brendanjamesgibson@gmail.com>
2020-05-07 16:33:18 -0700

    more updates to RSVP

--- src/js/rsvp.js
+++ src/js/rsvp.js
@@ -30,26 +30,46 @@ function getRSVP() {
 
 function displayRSVP(data) {
     var i;
+    let submissionChk = false;
     for (i = 0; i < data.length; i++) {
         if (data[i].Response == 'Pending') {
+            submissionChk = true;
             $("#formTitle").empty();
             $("#formTitle").text("Please RSVP By August 7th");
-            let card = $("<div class='form-group rsvp'></div>");
-            let id = $("<input class='_id' type='hidden' value='" + data[i]._oid + "'>");
-            let name = $("<h4>" + data[i].Guest + "</h4>");
-            let radioDiv = $("<div class='form-check form-check-inline'></div>");
-            let radioDivTwo = $("<div class='form-check form-check-inline'></div>");
-            let respY = $("<input type='radio' name='rsvp' id ='rsvpYes" + i + "'" + "value='Yes'>");
-            let labelY = $("<label class='form-check-label' for='rsvpYes" + i + "'" + ">Accepts With Pleasure</label>");
-            let respN = $("<input type='radio' name='rsvp' id ='rsvpNo" + i + "'" + "value='No'>");
-            let labelN = $("<label class='form-check-label' for='rsvpNo" + i + "'" + ">Regretfully Declines</label>");
-            radioDiv.append(respY, labelY);
-            radioDivTwo.append(respN, labelN);
-            card.append(id, name, radioDiv, radioDivTwo);
-            $("#rsvpForm").append(card);
+            renderRSVPFormCard(data[i], i);
             $("#responseDiv").show();
+        } else{
+            // TODO: Display RSVP status
         }
     }
+    if (submissionChk) {
+        let submissionBtn = $('<div class="form-group"><button type="submit" id="rsvpSubmitBtn" class="btn btn-default">Submit</button></div>')
+        $("#rsvpForm").append(submissionBtn);
+        //TODO
+    }
+}
+
+function renderRSVPFormCard(data, i) {
+    $("#formTitle").empty();
+    $("#formTitle").text("Please RSVP By August 7th");
+    let card = $("<div class='form-group rsvp'></div>");
+    let top = $("<div class='form-row guest text-left'><input class='_id' type='hidden' value='" + data._id + "'><h3>" + data.Guest + "</h3></div>");
+    let underline = $("<div class='row'><div class='col-sm-4 col marker'></div><div class='col-sm-7'></div></div>")
+    let radioRow = $("<div class='form-row rsvp-row'></div>");
+    let radioDiv = $("<div class='form-check form-check-inline col-sm-6'></div>");
+    let radioDivTwo = $("<div class='form-check form-check-inline col-sm-6'></div>");
+    let respY = $("<input type='radio' class='form-check-input' name='rsvp" + i + "' id ='rsvpYes" + i + "'" + "value='Yes''>");
+    let labelY = $("<label class='form-check-label' for='rsvpYes" + i + "'" + "> Accepts With Pleasure</label>");
+    let respN = $("<input type='radio' class='form-check-input' name='rsvp" + i + "' id ='rsvpNo" + i + "'" + "value='No'>");
+    let labelN = $("<label class='form-check-label' for='rsvpNo" + i + "'" + "> Regretfully Declines</label>");
+    let textRow = $("<div class='form-row rsvp-row text-left'><p style='color:#891632'>Please use this space to let us know if you have any dietary restrictions. Or just leave us a message!</p></div>");
+    let textArea = $("<textarea class='form-control' name='detailsTextArea" + i +  "' id='detailsTextArea" + i + "' rows='3'></textarea>")
+    radioDiv.append(respY, labelY);
+    radioDivTwo.append(respN, labelN);
+    radioRow.append(radioDiv, radioDivTwo);
+    textRow.append(textArea);
+    card.append(top, underline, radioRow, textRow);
+    $("#rsvpForm").append(card);
 }
 
 $(document).ready(function() {
