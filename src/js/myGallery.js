b09e886fe1d9b4a3528dac71dfc8c51dcfd872e4
bgibz <brendanjamesgibson@gmail.com>
2020-11-09 23:49:53 -0800

    added a plugin for thumbnails, removed splash page stuff from main css file

--- src/js/myGallery.js
+++ src/js/myGallery.js
@@ -4,24 +4,26 @@ lightGallery(document.getElementById('galleryOne'), {
 });
 */
 
-document.getElementById('launch-img').addEventListener('click', function() {
-    lightGallery(document.getElementById('launch-img'), {
-        thumbnail: true,
-        animateThumb: true,
-        showThumbByDefault: true,
-        dynamic: true,
-        dynamicEl: [{
-            "src": '../media/gallery/01.png',
-            'thumb': '../media/gallery/01.png',
-            'subHtml': '<h4>Test 1</h4><p>Testing stuff</p>'
-        }, {
-            'src': '../media/gallery/02.jpg',
-            'thumb': '../media/gallery/02.jpg',
-            'subHtml': "<h4>Test 2</h4><p>... still testing</p>"
-        }, {
-            'src': '../media/gallery/03.jpg',
-            'thumb': '../media/gallery/03.jpg',
-            'subHtml': "<h4>Test 3</h4><p>last test</p>"
-        }]
-      })
-    });
+document.getElementById("launch-img").addEventListener("click", function() {
+  lightGallery(document.getElementById("launch-img"), {
+    thumbnail: true,
+    dynamic: true,
+    dynamicEl: [
+      {
+        src: "../media/gallery/01.png",
+        thumb: "../media/gallery/01.png",
+        subHtml: "<h4>Test 1</h4><p>Testing stuff</p>"
+      },
+      {
+        src: "../media/gallery/02.jpg",
+        thumb: "../media/gallery/02.jpg",
+        subHtml: "<h4>Test 2</h4><p>... still testing</p>"
+      },
+      {
+        src: "../media/gallery/03.jpg",
+        thumb: "../media/gallery/03.jpg",
+        subHtml: "<h4>Test 3</h4><p>last test</p>"
+      }
+    ]
+  });
+});
