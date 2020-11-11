67d5dcb947dfb01f7acf1f694921c66ea5380c45
bgibz <brendanjamesgibson@gmail.com>
2020-11-10 21:54:45 -0800

    move engagement photos, add to gallery

--- src/js/myGallery.js
+++ src/js/myGallery.js
@@ -10,19 +10,106 @@ document.getElementById("launch-img").addEventListener("click", function() {
     dynamic: true,
     dynamicEl: [
       {
-        src: "../media/gallery/01.png",
-        thumb: "../media/gallery/01.png",
-        subHtml: "<h4>Test 1</h4><p>Testing stuff</p>"
+        src: "../media/engagement/1-engagement.jpg",
+        thumb: "../media/engagement/1-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><h6>Granville Island</h6><p>Sara Rogers Photography</p>"
       },
       {
-        src: "../media/gallery/02.jpg",
-        thumb: "../media/gallery/02.jpg",
-        subHtml: "<h4>Test 2</h4><p>... still testing</p>"
+        src: "../media/engagement/5-engagement.jpg",
+        thumb: "../media/engagement/5-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
       },
       {
-        src: "../media/gallery/03.jpg",
-        thumb: "../media/gallery/03.jpg",
-        subHtml: "<h4>Test 3</h4><p>last test</p>"
+        src: "../media/engagement/7-engagement.jpg",
+        thumb: "../media/engagement/7-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/8-engagement.jpg",
+        thumb: "../media/engagement/8-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/19-engagement.jpg",
+        thumb: "../media/engagement/19-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/20-engagement.jpg",
+        thumb: "../media/engagement/20-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/23-engagement.jpg",
+        thumb: "../media/engagement/23-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/24-engagement.jpg",
+        thumb: "../media/engagement/24-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/25-engagement.jpg",
+        thumb: "../media/engagement/25-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/28-engagement.jpg",
+        thumb: "../media/engagement/28-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/31-engagement.jpg",
+        thumb: "../media/engagement/31-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/32-engagement.jpg",
+        thumb: "../media/engagement/32-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/38-engagement.jpg",
+        thumb: "../media/engagement/38-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/42-engagement.jpg",
+        thumb: "../media/engagement/42-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/47-engagement.jpg",
+        thumb: "../media/engagement/47-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/63-engagement.jpg",
+        thumb: "../media/engagement/63-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
+      },
+      {
+        src: "../media/engagement/64-engagement.jpg",
+        thumb: "../media/engagement/64-engagement.jpg",
+        subHtml:
+          "<h4>Engagement Photo Session</h4><p>Sara Rogers Photography</p>"
       }
     ]
   });
